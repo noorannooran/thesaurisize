b8df74e5c5f0dd90210e13ec0f38d16d7a494a71
Nooran El-Sherif <nooran.elsherif@gmail.com>
2017-05-15 18:13:15 -0400

    move thesaurisize to thesaurisize01
    plan to build thesaurisize class in thesaurisize

--- thesaurisize.py
+++ thesaurisize.py
@@ -1,20 +1,2 @@
 from PyDictionary import PyDictionary
 import random
-
-dictionary = PyDictionary()
-
-print("Enter a sentence")
-sentence_input = input(">")
-sentence = sentence_input.split(' ')
-reconstructed = ''
-
-for word in sentence:
-    synonyms = dictionary.synonym(word)
-    if synonyms is None:
-        reconstructed += word + " "
-    else:
-        max_x = len(synonyms)
-        i = random.randint(0, max_x-1)
-        reconstructed += synonyms[i] + " "
-
-print(reconstructed)
